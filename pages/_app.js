0809287ea585e10737853a5896cafe18372efe09
Ales <aleswebgit@gmail.com>
2022-08-18 18:15:40 +0200

    base of newsapp

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NewsApp</title>
+        <meta name="description" content="newsapp - the best app to read news" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
