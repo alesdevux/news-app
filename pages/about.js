49889cc33e1f48500975647b3540d341d31df057
Ales <aleswebgit@gmail.com>
2022-08-18 18:37:00 +0200

    use pico.css and delete unused styles

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
-import styles from '../styles/Home.module.css'
-
 export default function About() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>About</h1>
       <p>This is the about page</p>
     </div>
