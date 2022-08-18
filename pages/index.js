722a190cd45f56ccfe12d24ea319e87290da745d
Ales <aleswebgit@gmail.com>
2022-08-18 19:43:18 +0200

    fetch newsapi on server side

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import PageLayout from '../components/PageLayout'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <PageLayout title="NewsApp - Home">
       <div className={styles.container}>
-        <h1>Aprendiendo Next.js desde 0</h1>
-        <Link href="/about">Ir a about</Link>
+        {articles.length === 0 && <p>loading</p>}
+        {articles.length > 0 && articles.map((article, index) => (
+          <div key={index}>
+            <h2>{article.title}</h2>
+            <img src={article.urlToImage} alt={article.title} />
+            <p>{article.description}</p>
+            <Link href="/article/[id]" as={`/article/${article.id}`}>
+              <a>Read more</a>
+            </Link>
+          </div>
+        ))}
       </div>
     </PageLayout>
   )
+}
+
+export async function getServerSideProps() {
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY
+  const response = await fetch(`https://newsapi.org/v2/everything?q=vegan&pageSize=25&sortBy=publishedAt&apiKey=${apiKey}`)
+  const data = await response.json()
+
+  return {
+    props: {
+      articles: data.articles
+    }
+  }
 }
