327ed7f72c5789848a46bdbacabfe924d1e7bb07
Ales <aleswebgit@gmail.com>
2022-08-18 18:22:34 +0200

    dynamic segment

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </div>
+  )
+}
---
+++ pages/article/[id].js
@@ -0,0 +1,10 @@
+export default function Article() {
+  return (
+    <div className="container">
+      <h1>This is an article</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim recusandae quidem id, exercitationem iure asperiores, excepturi officia animi explicabo consequuntur fugiat?
+      </p>
+    </div>
+  )
+}
